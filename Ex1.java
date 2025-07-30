d57da562c87fee8215d415ad96f6ea33db8a6798
priyadharshini222007 <abip25327@gmail.com>
2025-07-30 02:16:02 -0700

    Create Ex1.java

---
+++ Ex1.java
@@ -0,0 +1,19 @@
+package sample1;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		int sum=0;
+		
+	   for(int i=0;i<=100;i++)
+	   {
+		  sum += i;
+		  
+		   
+	   }
+	   System.out.println("The addition is:"+sum);
+			
+		}
+
+	}
+
+
